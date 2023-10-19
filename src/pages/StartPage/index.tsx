9710870c25dd7ae38bb532ecd339b6ff42b2c64e
LZTD1 <danil227pavlov@gmail.com>
2023-10-20 00:41:59 +0300

    Реализована стартовая страница

--- src/pages/StartPage/index.tsx
+++ src/pages/StartPage/index.tsx
@@ -4,11 +4,12 @@ import Wave from 'react-wavify';
 import logo from '../../assets/logo.jpg';
 import styles from '../StartPage/StartPage.module.scss';
 
-function index() {
+function index() : JSX.Element {
   return (
     <div className={styles.root}>
       <div className={styles.logoAndName}>
         <img src={logo.src} />
+        <button>Загрузка сайта...</button>
       </div>
       <div className={styles.waves}>
         <Wave
@@ -16,30 +17,24 @@ function index() {
           paused={false}
           style={{ display: 'flex' }}
           options={{
-            height: 500,
-            amplitude: 300,
+            height: 30,
+            amplitude: 140,
             speed: 0.05,
-            points: 4,
+            points: 6,
           }}
         />
-        {/* <Wave
+        <Wave
           fill="#1b5583"
           paused={false}
           style={{ display: 'flex' }}
           options={{
-            height: 300,
-            amplitude: 200,
+            height: 90,
+            amplitude: 90,
             speed: 0.1,
-            points: 5,
+            points: 4,
           }}
-        /> */}
+        />
       </div>
-      {/* <div
-        className="devBlock"
-        style={{
-          height: '100px',
-        }}
-      ></div> */}
     </div>
   );
 }
